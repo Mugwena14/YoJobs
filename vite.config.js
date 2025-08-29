ee8dedbe42d91ff8061d7ba84571b9b61f4bb1e3
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-29 17:07:09 +0200

    Blanck screen fix

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()],
-    base: process.env.VITE_BASE_PATH === 'production' ? '/YoJobs' : '/home',
+    base: process.env.VITE_BASE_PATH === 'production' ? '/YoJobs' : '/',
 })
