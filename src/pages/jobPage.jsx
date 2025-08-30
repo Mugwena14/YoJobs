44c6249e8bac73df558d2e2b003113a208666dbf
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-08-30 10:30:28 +0200

    Job fix

--- src/pages/jobPage.jsx
+++ src/pages/jobPage.jsx
@@ -1,50 +1,22 @@
+import Job from "../Components/Job"
 
-function JobPage({ job }) {
-    return (
-        <>
-        <Helmet>
-            <title>{job.title} – Apply on YoJobs</title>
-            <meta
-            name="description"
-            content={job.description.slice(0, 150)}
-            />
-            <meta property="og:title" content={job.title} />
-            <meta
-            property="og:description"
-            content={job.description.slice(0, 150)}
-            />
-            <meta property="og:url" content={`https://yojobs.vercel.app/job/${job.id}`} />
-            <meta property="og:type" content="job" />
+const job = () => {
 
-            {/* Schema.org JobPosting */}
-            <script type="application/ld+json">{JSON.stringify({
-            "@context": "https://schema.org/",
-            "@type": "JobPosting",
-            "title": job.title,
-            "description": job.description,
-            "datePosted": job.datePosted,
-            "validThrough": job.validThrough,
-            "employmentType": job.employmentType,
-            "hiringOrganization": {
-                "@type": "Organization",
-                "name": "YoJobs",
-                "sameAs": "https://yojobs.com"
+    async function deleting(jobId){
+        const res = await fetch(`http://localhost:3001/jobs/${jobId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
             },
-            "jobLocation": {
-                "@type": "Place",
-                "address": {
-                "@type": "PostalAddress",
-                "addressLocality": job.location,
-                "addressCountry": "ZA"
-                }
-            }
-            })}</script>
-        </Helmet>
+            body: JSON.stringify(jobId)
+        })
+    }
 
-        <h1>{job.title}</h1>
-        <p>{job.description}</p>
-        </>
-    );
+    return (
+        <div>
+            <Job DeleteJob={deleting}/>
+        </div>
+    )
 }
 
-export default JobPage;
+export default job
